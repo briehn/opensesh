d08603b79a915b63ad358f37e8f152e3045fe302
Brian Yu <briehnyu@gmail.com>
2023-03-14 18:26:18 -0400

    Add User Registration

--- backend/app.js
+++ backend/app.js
@@ -1,65 +1,64 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const debug = require('debug');
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const debug = require("debug");
 
-/*
-    CUSTOM IMPORTS
-*/
-    //CORS
-const cors = require('cors');
-const { isProduction } = require('./config/keys');
-    //CSRF
-const csurf = require('csurf');
+//MODELS
+require("./models/User");
 
-const usersRouter = require('./routes/api/users');
-const csrfRouter = require('./routes/api/csrf')
+//CORS
+const cors = require("cors");
+const { isProduction } = require("./config/keys");
+
+//CSRF
+const csurf = require("csurf");
+
+//ROUTERS
+const usersRouter = require("./routes/api/users");
+const csrfRouter = require("./routes/api/csrf");
 
 const app = express();
 
-app.use(logger('dev')); // log request components (URL/method) to terminal
+app.use(logger("dev")); // log request components (URL/method) to terminal
 app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
 if (!isProduction) {
-    app.use(cors());
-  }
+  app.use(cors());
+}
 
-  
 app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
 
-
-app.use('/api/users', usersRouter);
-app.use('/api/csrf', csrfRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/csrf", csrfRouter);
 
 // Express Error Logger
-const serverErrorLogger = debug('backend:error');
+const serverErrorLogger = debug("backend:error");
 
-  app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.statusCode = 404;
-    next(err);
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors,
   });
-
-  app.use((err, req, res, next) => {
-    serverErrorLogger(err);
-    const statusCode = err.statusCode || 500;
-    res.status(statusCode);
-    res.json({
-        message: err.message,
-        statusCode,
-        errors: err.errors
-    })
-  })
-
+});
 
 module.exports = app;
