4349f76057118f5a9f3b2281921b6912e1749d6f
Brian Yu <briehnyu@gmail.com>
2023-06-22 09:30:38 -0400

    More frontend support for friends

--- frontend/src/store/friendAction.js
+++ frontend/src/store/friendAction.js
@@ -1,4 +1,10 @@
 import jwtFetch from "./jwt";
+const RECEIVE_FRIEND_ERRORS = "friend/RECEIVE_FRIEND_ERRORS";
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_FRIEND_ERRORS,
+  errors,
+});
 
 export const addFriend = (friendId) => async (dispatch) => {
   try {
@@ -6,9 +12,11 @@ export const addFriend = (friendId) => async (dispatch) => {
       method: "POST",
     });
     const newFriend = await res.json();
-    dispatch(addFriend);
   } catch (err) {
-    console.log(err);
+    const resBody = await err.json();
+    if (resBody.statusCode === 400) {
+      dispatch(receiveErrors(resBody.errors));
+    }
   }
 };
 
@@ -17,7 +25,6 @@ export const removeFriend = (friendId) => async (dispatch) => {
     const res = await jwtFetch(`/api/friends/${friendId}`, {
       method: "DELETE",
     });
-    dispatch(removeFriend(friendId));
   } catch (err) {
     console.log(err);
   }
