0a35dec7f2a79c680565335c5504e91d4f1dd60f
Brian Yu <briehnyu@gmail.com>
2023-07-11 17:41:39 -0400

    Add styling, add main page functionality

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,16 +1,39 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { clearPostErrors, fetchPosts, clearPosts } from "../../store/posts";
 import PostBox from "../Posts/PostBox";
 
+import "./MainPage.css";
+
+function Heading({ filter, setFilter }) {
+  return (
+    <div className="filter-container">
+      <button
+        className={`filter pop ${!filter ? "active" : ""}`}
+        onClick={() => setFilter(false)}
+      >
+        Popular Posts
+      </button>{" "}
+      |{" "}
+      <button
+        className={`filter sesh ${filter ? "active" : ""}`}
+        onClick={() => setFilter(true)}
+      >
+        Your Sesh
+      </button>
+    </div>
+  );
+}
+
+const MemoizedHeading = React.memo(Heading);
+
 function MainPage() {
   const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.session.user);
-  const [filter, setFilter] = useState(true);
+  const [filter, setFilter] = useState(false);
   const posts = useSelector((state) =>
     state.posts.display ? Object.values(state.posts.display) : []
   );
-  const heading = filter ? "Your Sesh" : "Popular Posts";
 
   useEffect(() => {
     if (filter) {
@@ -24,25 +47,25 @@ function MainPage() {
     };
   }, [dispatch, filter, currentUser._id]);
 
-  console.log(posts.length);
-
   if (posts.length === 0) {
     return (
-      <>
-        <h2>{heading}</h2>
-        <div>No posts to display.</div>
+      <div className="mainbox-container">
+        <MemoizedHeading filter={filter} setFilter={setFilter} />
+        <div>No posts to display. Contact staff.</div>
         <footer>&copy; 2023 OpenSesh</footer>
-      </>
+      </div>
     );
   }
   return (
-    <>
-      <h2>{heading}</h2>
-      {posts.map((post) => (
-        <PostBox key={post._id} post={post} />
-      ))}
+    <div className="mainbox-container">
+      <MemoizedHeading filter={filter} setFilter={setFilter} />
+      <div className="posts-container">
+        {posts.map((post) => (
+          <PostBox key={post._id} post={post} />
+        ))}
+      </div>
       <footer>&copy; 2023 OpenSesh</footer>
-    </>
+    </div>
   );
 }
 
