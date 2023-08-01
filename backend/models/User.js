d44c83af54b05e617e895f34ac1bc8f8036fbb5f
Brian Yu <briehnyu@gmail.com>
2023-08-01 11:45:06 -0400

    Add backend support for new user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,6 +15,11 @@ const userSchema = Schema(
       type: String,
       required: true,
     },
+    answered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
   {
     timestamps: true,
