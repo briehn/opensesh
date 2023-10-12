04a2778a434c50be133b6024536d79739c59da26
Brian Yu <briehnyu@gmail.com>
2023-10-12 10:41:21 -0400

    More work done for prompt backend handling

--- backend/routes/api/prompts/prompt.js
+++ backend/routes/api/prompts/prompt.js
@@ -12,94 +12,91 @@ const CACHE_EXPIRATION_TIME = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
 const YEAR_IN_MS = 365 * CACHE_EXPIRATION_TIME;
 
 async function fetchQuestion() {
-  return new Promise((resolve, reject) => {
-    const validQuestions = questionsData.filter(
-      (question) => question.prompt.trim !== ""
+  if (
+    cachedPrompt &&
+    lastCachedTime &&
+    Date.now() - lastCachedTime < CACHE_EXPIRATION_TIME
+  ) {
+    return cachedPrompt;
+  }
+  const validQuestions = questionsData.filter(
+    (question) => question.prompt.trim !== ""
+  );
+
+  const eligibleQuestions = validQuestions.filter((question) => {
+    if (!question.lastUsed) {
+      return true;
+    }
+    const lastUsedDate = new Date(question.lastUsed);
+    const currentDate = new Date();
+
+    const timeSinceLastUsed = currentDate - lastUsedDate;
+    return timeSinceLastUsed >= YEAR_IN_MS;
+  });
+
+  if (eligibleQuestions.length === 0) {
+    const resetQuestions = validQuestions.map((question) => {
+      question.lastUsed = null;
+      return question;
+    });
+
+    fs.writeFile(
+      path.join(__dirname, "data.json"),
+      JSON.stringify(resetQuestions, null, 2),
+      (err) => {
+        if (err) {
+          console.error("Error writing JSON file:", err);
+          return "Internal Server Error Writing to AND Resetting JSON";
+        }
+      }
     );
 
-    const eligibleQuestions = validQuestions.filter((question) => {
-      if (!question.lastUsed) {
-        return true;
-      }
-      const lastUsedDate = new Date(question.lastUsed);
-      const currentDate = new Date();
+    fetchQuestion();
+  }
 
-      const timeSinceLastUsed = currentDate - lastUsedDate;
-      return timeSinceLastUsed >= YEAR_IN_MS;
-    });
+  const index = Math.floor(Math.random() * eligibleQuestions.length);
 
-    if (eligibleQuestions.length === 0) {
-      const resetQuestions = validQuestions.map((question) => {
-        question.lastUsed = null;
-        return question;
-      });
+  const sel = eligibleQuestions[index];
 
-      fs.writeFile(
-        path.join(__dirname, "data.json"),
-        JSON.stringify(resetQuestions, null, 2),
-        (err) => {
-          if (err) {
-            console.error("Error writing JSON file:", err);
-            return "Internal Server Error Writing to AND Resetting JSON";
-          }
-        }
-      );
+  sel.lastUsed = new Date().toISOString();
+
+  fs.readFile(path.join(__dirname, "data.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading JSON file:", err);
+      return "Internal Server Error Reading JSON";
     }
 
-    const index = Math.floor(Math.random() * eligibleQuestions.length);
+    const questions = JSON.parse(data);
 
-    const sel = eligibleQuestions[index];
-
-    sel.lastUsed = new Date().toISOString();
-
-    fs.readFile(path.join(__dirname, "data.json"), "utf8", (err, data) => {
-      if (err) {
-        console.error("Error reading JSON file:", err);
-        return "Internal Server Error Reading JSON";
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === sel.id) {
+        question.lastUsed = sel.lastUsed;
       }
-
-      const questions = JSON.parse(data);
-
-      const updatedQuestions = questions.map((question) => {
-        if (question.id === sel.id) {
-          question.lastUsed = sel.lastUsed;
-        }
-        return question;
-      });
-
-      fs.writeFile(
-        path.join(__dirname, "data.json"),
-        JSON.stringify(updatedQuestions, null, 2),
-        (err) => {
-          if (err) {
-            console.error("Error writing JSON file:", err);
-            return "Internal Server Error Writing to JSON";
-          }
-        }
-      );
+      return question;
     });
 
-    cachedPrompt = sel;
-    lastCachedTime = Date.now();
-    resolve(sel);
+    fs.writeFile(
+      path.join(__dirname, "data.json"),
+      JSON.stringify(updatedQuestions, null, 2),
+      (err) => {
+        if (err) {
+          console.error("Error writing JSON file:", err);
+          return "Internal Server Error Writing to JSON";
+        }
+      }
+    );
   });
+
+  cachedPrompt = sel.prompt;
+  lastCachedTime = Date.now();
+
+  return sel.prompt;
 }
 
 router.get("/", async (req, res, next) => {
   try {
-    console.log("CachedPrompt: " + cachedPrompt);
-    console.log("CachedTime: " + lastCachedTime);
-    console.log(
-      "Date Check: " + (Date.now() - lastCachedTime < CACHE_EXPIRATION_TIME)
-    );
-    if (
-      cachedPrompt &&
-      lastCachedTime &&
-      Date.now() - lastCachedTime < CACHE_EXPIRATION_TIME
-    ) {
-      return res.json({ prompt: cachedPrompt });
-    }
-    return res.json(fetchQuestion());
+    const prompt = await fetchQuestion();
+    return res.json({ prompt: prompt });
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Internal server error" });
